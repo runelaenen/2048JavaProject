39d1414695ab5b2c7ade32a0cb1a4f303f536dff
danbuze <dan.buze@student.kdg.be>
2015-02-06 22:29:32 +0100

    Wat opmerkingen in commentaar toegevoegd die we in de klas moeten zien op te lossen

--- src/be/kdg/spel/main.java
+++ src/be/kdg/spel/main.java
@@ -10,4 +10,10 @@ public class main {
     public static void main(String[] args) {
         new Controller();
     }
+
+    //problemen opgemerkt
+    // 1) als je focus op een knop zet door er op te klikken kan je niet meer met de pijltjes het spel spelen
+    // 2) soms als alle vakjes naar een kant moeten bv allemaal naar boven blijft er zoms een leeg vakje er tussen wat niet de bedoeling is
+    // 3) het spel lijkt precies nooit te eindigen( bord geraakt precies niet vol)
+
 }
